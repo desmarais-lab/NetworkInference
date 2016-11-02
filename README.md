0571ae23a36ec9b8fe56d6fd23ade5e8be9f0858
Fridolin Linder <fridolin.linder@gmail.com>
2016-11-02 10:21:47 -0400

    snap core lib compiles and links successfully

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # netinf
 R implementation of the netinf algorithm
+
+
+
+
+### Notes:
+- in `snap/snap-core/table.cpp` line 764 commented out LoadSSPar, threw not
+    declared exception. Figure out what's up with that (11/2/)
--- src/netinf.cpp
+++ src/netinf.cpp
@@ -16,12 +16,3 @@ NumericVector timesTwo(NumericVector x) {
   return x * 2;
 }
 
-
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically 
-// run after the compilation.
-//
-
-/*** R
-timesTwo(42)
-*/
