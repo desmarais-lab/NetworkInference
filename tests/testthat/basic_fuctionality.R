53df0070862053affa73e537ff7af64432b68538
Fridolin Linder <fridolin.linder@gmail.com>
2016-11-04 14:20:52 -0400

    adds basic functionality unit test

--- tests/testthat/basic_fuctionality.R
+++ tests/testthat/basic_fuctionality.R
@@ -1,7 +1,10 @@
-library(netinf)
+library(NetworkInference)
 
 context("Basic functionality test")
 
 test_that("netinf_test produces same file as original netinf executable", {
-    
+    original <- readLines('data/original_output_network.txt')
+    test_netinf()
+    from_test_netinf <- readLines('data/test_out.txt')
+    expect_identical(original, from_test_netinf)
 })
