2428a706080b8abaef4c981178fd5b1d2f7ea3ec
flinder <fridolin.linder@gmail.com>
2016-11-12 13:34:57 -0500

    added skeleton for some generics

---
+++ R/plot.R
@@ -0,0 +1,11 @@
+#' Plot a cascade object
+#' 
+#' @import ggplot2
+#' 
+#' @param cascades Object of class cascade to be plottet
+#' @param ... Additional arguments passed to ggplot
+#' 
+#' @return A ggplot plot object
+plot.cascade <- function(cascade) {
+    .NotYetImplemented()
+}
---
+++ R/summary.R
@@ -0,0 +1,8 @@
+#' Summarize cascade object
+#' 
+#' @param cascades Object of class cascade to be plottet
+#' @return Prints cascade summary information to the screen and returns summary
+#'     stats in a list
+summary.cascade <- function(cascades) {
+    .NotYetImplemented()
+}
