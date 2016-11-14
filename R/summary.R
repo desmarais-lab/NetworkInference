9c38fd7502608ea56e27b048455b847dae64b1a6
Fridolin Linder <fridolin.linder@gmail.com>
2016-11-14 10:49:51 -0500

    adds draft summary method for cascade objects

--- R/summary.R
+++ R/summary.R
@@ -4,5 +4,10 @@
 #' @return Prints cascade summary information to the screen and returns summary
 #'     stats in a list
 summary.cascade <- function(cascades) {
-    .NotYetImplemented()
+    # Cascade info
+    casc_lengths <- sapply(cascades$cascade_ids, length)
+    names(casc_lengths) <- NULL
+    casc_names <- names(cascades$cascade_ids)
+    casc_info <- data.frame("name" = casc_names, "length" = casc_lengths)
+    return(casc_info)
 }
