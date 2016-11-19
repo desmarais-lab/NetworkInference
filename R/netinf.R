1507b00d9ac44dc83a75612af7091f8bc6f56b3c
flinder <fridolin.linder@gmail.com>
2016-11-19 18:46:25 -0500

    Implements edge_info argument

--- R/netinf.R
+++ R/netinf.R
@@ -11,15 +11,20 @@
 #' @import assertthat
 #' 
 #' @param  cascades An object of class cascade containing node and cascade 
-#'     information. See \link{as.cascade} for details.
+#'     information. See \link{as.cascade} for details. 
 #' @param trans_mod character, indicating the choice of model: 
 #'      \code{"exponential"}, \code{"power"} (power law) or \code{"rayleigh"}.
 #' @param alpha Numeric, alpha for transmission model.
 #' @param n_iter Numeric, number of iterations for optimization.
 #' @param verbose logical, should additional output be printed.
+#' @param edge_info logical, should additional information on each edge be 
+#'    returned.
 #' 
 #' @return Returns the inferred diffusion network as an object of class 
-#'  \link[base]{matrix} of dimension \code{k x 2} where k is the number of edges.
+#'  \link[base]{data.frame}. If \code{edge_info = FALSE} of dimension \code{k x 2} 
+#'      where k is the number of edges. If \code{edge_info = TRUE} additional
+#'      columns contain information on volume, marginal gain and time difference
+#'      for the respective edge. 
 #'  
 #' @references 
 #' M. Gomez-Rodriguez, J. Leskovec, A. Krause. Inferring Networks of Diffusion 
@@ -32,7 +37,7 @@
 #'               
 #' @export
 netinf <- function(cascades, trans_mod = "exponential", alpha = 1.0, n_iter = 5,
-                   verbose = TRUE) {
+                   verbose = TRUE, edge_info = TRUE) {
     
     # Check inputs 
     assert_that(class(cascades)[1] == "cascade")
@@ -57,17 +62,26 @@ netinf <- function(cascades, trans_mod = "exponential", alpha = 1.0, n_iter = 5,
     cascade_nodes <- lapply(cascades$cascade_nodes, function(x) node_ids[x]) 
     
     # Run netinf
-    netinf_out <- netinf_(node_ids = node_ids, 
-                          node_names = cascades$node_names, 
+    netinf_out <- netinf_(node_ids = node_ids, node_names = cascades$node_names, 
                           cascade_ids = cascade_nodes, 
-                          cascade_times = cascades$cascade_times, 
-                          model = model, alpha = alpha, n_iter = n_iter, 
-                          verbose = verbose)
+                          cascade_times = cascades$cascade_times, model = model, 
+                          alpha = alpha, n_iter = n_iter, verbose = verbose, 
+                          edge_info = edge_info)
+    
+    # Concatenate and order by origin id 
+    out <- as.data.frame(do.call(rbind, netinf_out))
+    out <- out[order(out[, 1]), ]
     
     # Replace integer node_ids with node_names
-    netinf_out <- do.call(rbind, netinf_out)
-    origin <- cascades$node_names[netinf_out[, 1]]
-    destin <- cascades$node_names[netinf_out[, 2]]
-    out <- data.frame("origin_node" = origin, "destination_node" = destin) 
+    out[, 1] <- cascades$node_names[out[, 1]]
+    out[, 2] <- cascades$node_names[out[, 2]]
+    
+    if(edge_info) {
+        colnames(out) <- c("origin_node", "destination_node", "volume", 
+                           "marginal_gain", "median_time_difference",
+                           "mean_time_difference")
+    } else {
+        colnames(out) <- c("origin_node", "destination_node")
+    }
     return(out)
 }
