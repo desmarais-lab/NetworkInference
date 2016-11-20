a0f5d01842bd2ea8c08e0d9c4f78468b6920e40e
flinder <fridolin.linder@gmail.com>
2016-11-19 19:28:18 -0500

    Order netinf output by first two not one cols

--- R/netinf.R
+++ R/netinf.R
@@ -70,11 +70,11 @@ netinf <- function(cascades, trans_mod = "exponential", alpha = 1.0, n_iter = 5,
     
     # Concatenate and order by origin id 
     out <- as.data.frame(do.call(rbind, netinf_out))
+    out <- out[order(out[, 1], out[, 2]), ]
     
     # Replace integer node_ids with node_names
     out[, 1] <- cascades$node_names[out[, 1]]
     out[, 2] <- cascades$node_names[out[, 2]]
-    out <- out[order(out[, 1]), ]
     
     if(edge_info) {
         colnames(out) <- c("origin_node", "destination_node", "volume", 
@@ -83,5 +83,6 @@ netinf <- function(cascades, trans_mod = "exponential", alpha = 1.0, n_iter = 5,
     } else {
         colnames(out) <- c("origin_node", "destination_node")
     }
+    rownames(out) <- c(1:nrow(out))
     return(out)
 }
