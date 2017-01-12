0c1517d1d5966bd9954a19dbb0153fe3aff941a5
Fridolin Linder <fridolin.linder@gmail.com>
2017-01-12 11:16:20 -0500

    Reactivate netinf tests

--- tests/testthat/test_netinf.R
+++ tests/testthat/test_netinf.R
@@ -7,29 +7,14 @@ data(validation)
 test_that("netinf produces the edges as original netinf executable.", {
     from_netinf <- netinf(cascades, lambda = 1, trans_mod = "exponential",
                           n_edges = 5)
-    ncascades = length(cascades$cascade_times)
-    # reshape cascade data to work with netinf.fun
-    cascade_data = NULL
-    for(i in 1:ncascades) {
-      datai = data.frame(i,cascades$cascade_nodes[[i]],cascades$cascade_times[[i]],stringsAsFactors=F)
-      cascade_data = rbind(cascade_data,datai)
-    }
-    
-    # Run and compare
-    netinf_results = netinf.fun(cascade_data,n.edges=6,lambda=1,beta=.5,epsilon=0.000000001)
-    
-    
-    expect_equal(validation[, -c(3:6)], from_netinf)
-})
-test_that("netinf produces the same edges and edge information as original.", {
-    from_netinf <- netinf(cascades, alpha = 1, trans_mod = "exponential",
-                          verbose = FALSE, edge_info = TRUE)
-    # Round double precision numbers for comparison
-    from_netinf$mean_time_difference <- round(from_netinf$mean_time_difference, 6)
-    from_netinf$median_time_difference <- round(from_netinf$median_time_difference, 6)
-    from_netinf$marginal_gain <- round(from_netinf$marginal_gain, 6)
-    validation$mean_time_difference <- round(validation$mean_time_difference, 6)
-    validation$median_time_difference <- round(validation$median_time_difference, 6)
-    validation$marginal_gain <- round(validation$marginal_gain, 6)
-    expect_equal(validation, from_netinf)
+    t1 <- from_netinf[order(from_netinf[, 1], from_netinf[, 2]), -3]
+    rownames(t1) <- c(1:nrow(t1))
+    rownames(t1) <- c(1:nrow(t1))
+    rownames(t1) <- c(1:nrow(t1))
+    rownames(t1) <- c(1:nrow(t1))
+    rownames(t1) <- c(1:nrow(t1))
+    rownames(t1) <- c(1:nrow(t1))
+    t2 <- validation[order(validation[, 1], validation[, 2]), -c(3:6)]
+    rownames(t2) <- c(1:nrow(t2))
+    expect_equal(t1, t2)
 })
