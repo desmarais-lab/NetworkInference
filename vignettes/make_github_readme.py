76e4dce7ca2cc992cca551698e789e25881db5ab
Fridolin Linder <fridolin.linder@gmail.com>
2017-01-20 10:22:06 -0500

    dont delete html when creating readme

--- vignettes/make_github_readme.py
+++ vignettes/make_github_readme.py
@@ -6,7 +6,7 @@ import shutil
 INFILE = 'netinf_vignette.Rmd'
 OUTFILE = 'readme.Rmd'
 
-readme_header = '---\ntitle: "NetworkInference"\nauthor: "Fridolin Linder"\ndate: "`r Sys.Date()`"\noutput: rmarkdown::github_document\nbibliography: bibliography.bib\n---\n![](https://travis-ci.org/flinder/NetworkInference.svg)'
+readme_header = '---\ntitle: "NetworkInference"\nauthor: "Fridolin Linder"\ndate: "`r Sys.Date()`"\noutput: rmarkdown::github_document\nbibliography: bibliography.bib\n---\n\n[//]: # (This is an automatically generated document. If you want to change it please make changes in `vignettes/netinf_vignette.Rmd` and then run `vignettes/make_github_readme.py`. This will automatically generate this readme document.)\n\n![](https://travis-ci.org/flinder/NetworkInference.svg)'
 
 # Read vignette
 with open(INFILE, 'r') as infile, open(OUTFILE, 'w') as outfile:
@@ -20,7 +20,7 @@ process.wait()
 if os.path.isdir('../readme_files'):
     shutil.rmtree('../readme_files')
 
-shutil.move('readme_files', '../readme_files')
+shutil.move('readme_files/', '../readme_files')
 os.rename("readme.md", "../README.md")
-os.remove("readme.html")
+#os.remove("readme.html")
 os.remove("readme.Rmd")
