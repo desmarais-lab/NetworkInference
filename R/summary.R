e714331606c47f888022078632468f5de814a790
Fridolin Linder <fridolin.linder@gmail.com>
2017-02-22 11:06:57 -0500

    Summary now prints nice summary stats

--- R/summary.R
+++ R/summary.R
@@ -1,21 +1,61 @@
 #' Summarize cascade object
 #' 
 #' @param object Object of class cascade to be summarized.
+#' @param quiet logical, if \code{FALSE} summary stats are printed to std out.
 #' @param ... Additional arguments passed to summary
-#' @return Prints cascade summary information to the screen and returns summary
-#'     stats in a list
+#' @return Prints cascade summary information to the screen 
+#'     (if \code{quiet = FALSE}). \code{'# cascades'} is the number of cascades in
+#'     the object, \code{'# nodes'} is the number of nodes in the system (nodes 
+#'     that can theoretically experience an event), \code{'# nodes in cascades'} is 
+#'     the number of unique nodes of the system that experienced an event and 
+#'     \code{'# possible edges'} is the number of edges that are possible given
+#'     the cascade data (see \code{\link{count_possible_edges}} for details.). 
+#'     
+#'     Additional summaries for each cascade are returned invisibly. 
+#      as a \code{data.frame} with columns \code{name} (name of the 
+#'     cascade), \code{length} (length of the cascade as an integer of how many
+#'     nodes experienced and event) and \code{n_ties} (number of tied event 
+#'     times per cascade).
 #'     
 #' @examples 
-#' 
 #' data(cascades)
 #' summary(cascades)
 #' 
 #' @export
-summary.cascade <- function(object, ...) {
+summary.cascade <- function(object, quiet = FALSE, ...) {
     # Cascade info
     casc_lengths <- sapply(object$cascade_nodes, length)
+    dups <- sapply(object$cascade_times, count_dups_)
     names(casc_lengths) <- NULL
     casc_names <- names(object$cascade_nodes)
-    casc_info <- data.frame("name" = casc_names, "length" = casc_lengths)
-    return(casc_info)
+    casc_info <- data.frame("name" = casc_names, "length" = casc_lengths,
+                            "n_ties" = dups)
+    # Print out summaries 
+    if(!quiet) {
+        # Calculate all summaries
+        n_nodes <- length(object$node_names)
+        n_nodes_in_casc <- length(unique(do.call(c, object$cascade_nodes)))
+        npe <- count_possible_edges(cascades)
+        pout <- cbind(summary(casc_lengths), summary(dups))
+        colnames(pout) <- c("length", "ties")
+        cat(paste0('# cascades: ', length(object$cascade_times), '\n'))
+        cat(paste0('# nodes: ', n_nodes, '\n'))
+        cat(paste0('# nodes in cascades: ', n_nodes_in_casc, '\n'))
+        cat(paste0('# possible edges: ', npe, '\n\n'))
+        cat("Summary statistics for node length and ties:\n") 
+        print(pout)
+    }
+     
+    return(invisible(casc_info))
 }
+
+
+#' Count duplicates in a vector
+#' 
+#' @param x vector to count duplicates in
+#' 
+#' @return An integer count of the number of duplicated values.
+count_dups_ <- function(x) {
+    return(length(x) - length(unique(x)))
+}
+
