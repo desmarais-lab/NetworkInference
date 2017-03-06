552fe7daf6af8c5f34220414894fc9c57008ba3b
Fridolin Linder <fridolin.linder@gmail.com>
2017-03-06 15:13:47 -0500

    Implememnts partial cascades in `simulate_cascade`
    Closes #36

--- R/simulate_cascades.R
+++ R/simulate_cascades.R
@@ -68,11 +68,13 @@ simulate_rnd_cascades <- function(n_cascades, n_nodes, id_class = "character") {
 #' @param epsilon Weight for out of network diffusion
 #' @param model Diffusion model to use. One of \code{c("exponential", 
 #'     "rayleigh")}.
+#' @param partial_cascade Object of type cascade, containing one partial 
+#'     cascades for which further development should be simulated.
 #' @param max_time Numeric, the maximum time after which observations are 
 #'     censored
 #'     
 #' @return A data frame containing (in order of columns) node ids, 
-#'     event time and cascade identifier.
+#'     event time and cascade identifier. 
 #'     
 #' @examples
 #' 
@@ -81,24 +83,34 @@ simulate_rnd_cascades <- function(n_cascades, n_nodes, id_class = "character") {
 #' simulated_cascades <- simulate_cascades(out, nsim = 10, lambda = 1, 
 #'                                         beta = 0.5, epsilon = 10^-9, 
 #'                                         model = "exponential")
+#'  
+#' # Simulation from partial cascade
+#' 
 #' @export
 simulate_cascades <- function(diffnet, nsim = 1, seed = NULL, max_time = Inf,
-                              lambda, beta, epsilon, model) {
+                              lambda, beta, epsilon, model, 
+                              partial_cascade = NULL) {
     
     # Check inputs
     assert_that(is.diffnet(diffnet))
     assert_that(nsim >= 1)
     model <- match.arg(model, c("exponential", "rayleigh"))
     
-    # Initialize data structures 
-    set.seed(seed)
-    
-    ## Nodes and number of nodes
+    # Extract Nodes and number of nodes
     nodes <- unique(c(diffnet$origin_node, diffnet$destination_node))
     n_nodes <- length(nodes)
     
+    # Check partial cascade input 
+    if(!is.null(partial_cascade)) {
+        assert_that(is.cascade(partial_cascade))
+        assert_that(length(partial_cascade$cascade_nodes) == 1)
+        assert_that(length(partial_cascade$cascade_times) == 1)
+        assert_that(all(is.element(partial_cascade$cascade_nodes[[1]], nodes)))
+    }
+    
+    set.seed(seed)
+   
     ## Create adjacency matrix from edgelist (ordered as in nodes, row -> sender)
-    ## with diffusion edge probabilities (normalized epsilon where no edge)
     X_ <- matrix(0, ncol = n_nodes, nrow = n_nodes)
     for(k in 1:nrow(diffnet)) {
         i <- which(nodes == diffnet[k, 1])
@@ -108,15 +120,17 @@ simulate_cascades <- function(diffnet, nsim = 1, seed = NULL, max_time = Inf,
     
     sim_out <- lapply(X = 1:nsim, FUN = simulate_cascade_, n_nodes = n_nodes, 
                       lambda = lambda, epsilon = epsilon, max_time = max_time, 
-                      model = model, nodes = nodes, beta = beta, X_ = X_)
+                      model = model, nodes = nodes, beta = beta, X_ = X_, 
+                      partial_cascade = partial_cascade)
     out <- do.call(rbind, sim_out)
     rownames(out) <- NULL
-    return(out)
-}
+    return(out) }
 
-# Simulate a single cascade
+
+# Simulate a single cascade from scratch (random first event node)
 simulate_cascade_ <- function(i, nodes, n_nodes, lambda, epsilon, max_time, 
-                              model, beta, X_) {
+                              model, beta, X_, partial_cascade) {
+    
     # Generate relative diffusion times for all pairs
     if(model == "exponential") {
         rel_diff_times <- matrix(stats::rexp(n_nodes^2, rate = lambda), nrow = n_nodes)
@@ -126,6 +140,11 @@ simulate_cascade_ <- function(i, nodes, n_nodes, lambda, epsilon, max_time,
     
     # No diffusion of node to itself
     diag(rel_diff_times) <- 0
+    if(!is.null(partial_cascade)) {
+        # No diffusion to nodes that already had an event in partial_cascade
+        idx <- which(is.element(nodes, partial_cascade$cascade_nodes[[1]]))
+        rel_diff_times[, idx] <- 0       
+    }
     # Censor at maximum observation time
     rel_diff_times[rel_diff_times > max_time] <- 0
     
@@ -135,20 +154,36 @@ simulate_cascade_ <- function(i, nodes, n_nodes, lambda, epsilon, max_time,
     # Set colums to 1 with probability norm_ep
     Y[, as.logical(stats::rbinom(n_nodes, 1, prob = norm_epsilon))] <- 1
     
-    # Set relative diffusion times with no edge to 0 
+    # Set relative diffusion times between nodes with no corresponding diffusion
+    # -edge to 0 
     rel_diff_times <- (X_ - Y)^2 * rel_diff_times
     rownames(rel_diff_times) <- colnames(rel_diff_times) <- nodes
     
-    start_node <- sample(nodes, 1)
+    if(is.null(partial_cascade)) {
+        start_nodes <- sample(nodes, 1)
+    } else {
+        start_nodes <- nodes[idx]
+    }
     # Find shortest path from start node to every other (reachable) node
     g <- igraph::graph.adjacency(rel_diff_times, weighted = TRUE, 
                                  mode = "directed")
-    dists <- igraph::distances(g, v = start_node, mode = "out")
+    dists <- igraph::distances(g, v = start_nodes, mode = "out")
     
-    # Isolates will have infinite infection time (remove)
-    dists <- dists[, !is.infinite(dists), drop = FALSE]
-    out <- data.frame("node_name" = colnames(dists), 
-                      "event_time" = as.numeric(dists),
+    if(is.null(partial_cascade)) {
+        prev_event_times <- 0
+    } else {
+        prev_event_times <- partial_cascade$cascade_times[[1]]
+    }
+    # Find the earliest event time for each node
+    ## Add the event time of each node in partial_cascade to shortest path 
+    ## from this node (or add 0 if no partial cascade)
+    abs_dists <- t(sapply(1:nrow(dists), 
+                          function(i) dists[i, ] + prev_event_times[i]))
+    d <- apply(abs_dists, 2, min) 
+    d <- d[!is.infinite(d)]
+    
+    out <- data.frame("node_name" = names(d), 
+                      "event_time" = as.numeric(d),
                       "cascade_id" = i)
     return(out)
 }
--- tests/testthat/test_simulation.R
+++ tests/testthat/test_simulation.R
@@ -1,7 +1,7 @@
 library(NetworkInference)
 library(igraph)
 
-context("Test if core netinf method works")
+context("Test if simulation methods works")
 
 data(cascades)
 test_that("Simulation function works.", {
@@ -18,4 +18,17 @@ test_that("Simulation function works.", {
     from_netinf <- from_netinf[order(as.numeric(from_netinf[, 2])), ]
     rownames(rec) <- rownames(from_netinf) <- NULL
     expect_equal(from_netinf[, -3], rec[, -3])
+})
+test_that("Simulation function with partial cascade works.", {
+    partial_cascade <- cascades
+    partial_cascade$cascade_nodes <- cascades$cascade_nodes[10]
+    partial_cascade$cascade_times <- cascades$cascade_times[10]
+    from_netinf <- netinf(cascades, lambda = 1, trans_mod = "exponential",
+                          n_edges = 100)   
+    out <- simulate_cascades(from_netinf, nsim = 100, seed = 123, max_time = 10, 
+                             lambda = 1, beta = 0.5, epsilon = 10e-9, 
+                             model = "exponential", 
+                             partial_cascade = partial_cascade)
+    expect_equal(length(unique(out[out$node_name == "22", 2])), 1)
+    expect_equal(length(unique(out[out$node_name == "28", 2])), 1)
 })
