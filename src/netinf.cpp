012c11dce0b0eb16861564811623446fa4fcd817
flinder <fridolin.linder@gmail.com>
2018-02-01 15:06:43 -0500

    minor: fixed indentation

--- src/netinf.cpp
+++ src/netinf.cpp
@@ -120,7 +120,7 @@ Rcpp::List optimal_spanning_tree_(Rcpp::IntegerVector &this_cascade_ids,
             double score;
             for (int k = 0; k < n_parents; k++) {
                 score = edge_weight_(parent_times[k], this_cascade_times[i],
-                                            lambda, beta, epsilon, false, model);
+                                     lambda, beta, epsilon, false, model);
                 if (score > max_parent_score) {
                     max_parent_score = score;
                     parent = possible_parents[k];
